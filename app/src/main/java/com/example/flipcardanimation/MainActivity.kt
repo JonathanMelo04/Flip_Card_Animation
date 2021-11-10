9d6ced16a3aaf960251453daac1d30086ef4126c
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-10 20:10:12 -0300

    flash card implementation

--- app/src/main/java/com/example/flipcardanimation/MainActivity.kt
+++ app/src/main/java/com/example/flipcardanimation/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.flipcardanimation
-
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.flipcardanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMainActivity: ActivityMainBinding
+    lateinit var back_anim: AnimatorSet
+    lateinit var front_anim: AnimatorSet
+    var isFront = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMainActivity.root)
+
+        val scale: Float = applicationContext.resources.displayMetrics.density
+
+        bindingMainActivity.cardFront.cameraDistance = 8000 * scale
+        bindingMainActivity.cardBack.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.front_animator) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.back_animator) as AnimatorSet
+
+        bindingMainActivity.btnFlip.setOnClickListener {
+            if(isFront){
+                front_anim.setTarget(bindingMainActivity.cardFront)
+                back_anim.setTarget(bindingMainActivity.cardBack)
+                front_anim.start()
+                back_anim.start()
+                isFront = false
+            }else{
+                front_anim.setTarget(bindingMainActivity.cardBack)
+                back_anim.setTarget(bindingMainActivity.cardFront)
+                front_anim.start()
+                back_anim.start()
+                isFront = true
+            }
+        }
     }
 }
